1eb8a2013f1a3c95b9bbb2c6023e9b3571bbd114
Jeni2505 <2123200440@soy.utj.edu.mx>
2026-02-24 00:17:58 -0600

    Descripción de los cambios

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'todo-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+// Instalación del Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Estrategia para cargar archivos si no hay conexión
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {VitePWA} from "vite-plugin-pwa"
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
-      manifest:{
-        name:"Todo App",
-        short_name: "Todo",
-        description: "Una aplicacion de tareas simple",
-        start_url: "./",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#963fb5",
-        icons:[
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Todo App',
+        short_name: 'Todo',
+        description: 'Una aplicacion de tareas simple',
+        start_url: './',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#963fb5',
+        icons: [
           {
-            src: '/icons/icon192x192.png',
+            src: '/icons/icon-192x192.png',
             sizes: '192x192',
-            type: "image/png"
+            type: 'image/png'
           },
-           {
-            src: '/icons/icon512x512.png',
+          {
+            src: '/icons/icon-512x512.png',
             sizes: '512x512',
-            type: "image/png"
+            type: 'image/png'
           }
         ],
         screenshots: [
           {
-            src: '/screenshots/Captura 1.png',
+            src: '/screenshots/Captura1.png',
             sizes: '783x780',
-            type: 'image/jpg',
+            type: 'image/jpg'
           }
-        ],
+        ]
       },
+
+      // 🔥 IMPORTANTE: desactivar en desarrollo
       devOptions: {
-        enabled: true
-      },
-    }),
-  
-  
-  ],
-});
+        enabled: false
+      }
+    })
+  ]
+})
